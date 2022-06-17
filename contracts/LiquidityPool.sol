9db533fb3f3dcb9b383ec18e3c2678e6c44ce417
Julius Raynaldi <jraynaldi3@gmail.com>
2022-06-17 15:16:07 +0700

    add balance and price function

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.4;
 
+import "./interface/ILiquidityFactory.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 contract LiquidityPoll {
@@ -10,7 +12,35 @@ contract LiquidityPoll {
         address token2;
     }
 
+    uint fee = 25;
+
     Liquidity liquidityPool;
     constructor() {
+        (liquidityPool.token1, liquidityPool.token2) = ILiquidityFactory(msg.sender).params();
     }
+
+    function balance1() internal view returns(uint){
+        (bool success, bytes memory data) = 
+            liquidityPool.token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+        require(success && data.length >= 32);
+        return abi.decode(data , (uint));
+    }
+
+    function balance2() internal view returns(uint){
+        (bool success, bytes memory data) = 
+            liquidityPool.token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+        require(success && data.length >= 32);
+        return abi.decode(data , (uint));
+    }
+
+    function getPrice(uint amount1, uint amount2) internal pure returns(uint){
+        return amount1/amount2;
+    }
+
+    function getCurrentPrice() public view returns(uint price){
+        uint token1 = balance1();
+        uint token2 = balance2();
+        price = getPrice(token1, token2);
+    }
+
 }
