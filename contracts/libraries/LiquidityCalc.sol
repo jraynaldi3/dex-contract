718a328330fcf32d324cc387cd11d3c8b0e7b597
Julius Raynaldi <jraynaldi3@gmail.com>
2022-06-24 14:23:58 +0700

    add comment

--- contracts/libraries/LiquidityCalc.sol
+++ contracts/libraries/LiquidityCalc.sol
@@ -2,11 +2,19 @@
 
 pragma solidity ^0.8.4;
 
+///@notice for calculate liquidity 
 library LiquidityCalc {
+    ///@notice convert an amount number of transfer to liquidity 
+    ///@dev use while adding liquidity to pool
+    ///@param amount of token;
+    ///@param balance token balance of liquidity pool;
+    ///@param totalLiquidity total liquidity that pool have;
     function amountToLiquidity(uint amount, uint balance, uint totalLiquidity) internal pure returns(uint liquidity){
         liquidity = amount / balance * totalLiquidity;
     }
 
+    ///@notice convert liquidity to amount of token;
+    ///@dev use while removing liquidity from pool
     function liquidityToAmount(uint liquidity, uint balance, uint totalLiquidity) internal pure returns(uint amount){
         amount = liquidity / totalLiquidity * balance;
     }
