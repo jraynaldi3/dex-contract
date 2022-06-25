252fa76f18d0b1d4a6d606586bd209bdfddcba40
Julius Raynaldi <jraynaldi3@gmail.com>
2022-06-25 11:37:39 +0700

    cleaner code with TransferHelper.sol
    remove liquidityPool struct and using transfer helper to make code look cleaner and readable

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -2,11 +2,12 @@
 
 pragma solidity ^0.8.4;
 
-import "./interface/ILiquidityFactory.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interface/ILiquidityFactory.sol";
 import "./interface/ILiquidityPool.sol";
 import "./libraries/LiquidityCalc.sol";
+import "./libraries/TransferHelper.sol";
 
 
 contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
@@ -15,11 +16,9 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
     event LiquidityRemoved(uint amount1, uint amount2, address by);
     event Swap(uint amount1, uint amount2, address by, uint atPrice);
     
-    struct Liquidity {
-        address token1;
-        address token2;
-    }
-
+    
+    address token1;
+    address token2;
     uint fee = 25;
     uint space = 1000;
     uint deliminator = 2**64;
@@ -27,23 +26,22 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
 
     mapping (address => uint) public override liquidityOfAccount;
 
-    Liquidity liquidityPool;
     constructor() {
-        (liquidityPool.token1, liquidityPool.token2) = ILiquidityFactory(msg.sender).params();
+        (token1, token2) = ILiquidityFactory(msg.sender).params();
     }
 
 
 
     function balance1() internal view returns(uint){
         (bool success, bytes memory data) = 
-            liquidityPool.token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+            token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
         require(success && data.length >= 32);
         return abi.decode(data , (uint));
     }
 
     function balance2() internal view returns(uint){
         (bool success, bytes memory data) = 
-            liquidityPool.token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+            token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
         require(success && data.length >= 32);
         return abi.decode(data , (uint));
     }
@@ -56,9 +54,9 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
         if (totalLiquidity <= 0) {
             return price = 0;
         }
-        uint token1 = balance1();
-        uint token2 = balance2();
-        price = getPrice(token1, token2);
+        uint balanceToken1 = balance1();
+        uint balanceToken2 = balance2();
+        price = getPrice(balanceToken1, balanceToken2);
     }
 
     function getPriceAfterSwap(uint amount, bool oneToTwo) public view override returns(uint price){
@@ -78,12 +76,8 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
         if(totalLiquidity > 0){
             require ( getPrice(amount1, amount2)> price - (price*space/10000) && getPrice(amount1, amount2) < price + (price*space/10000),"Wrong Price");
         }
-        (bool success, ) = 
-            liquidityPool.token1.call(abi.encodeWithSignature("transferFrom(address,address,uint256)", address(msg.sender),address(this), amount1));
-        require(success,"Transfer Unsuccessfull");
-        (bool success2, ) = 
-            liquidityPool.token2.call(abi.encodeWithSignature("transferFrom(address,address,uint256)", address(msg.sender),address(this), amount2));
-        require(success2,"Transfer Unsuccessfull");
+        TransferHelper.transferFrom(token1, address(msg.sender), address(this), amount1);
+        TransferHelper.transferFrom(token2, address(msg.sender), address(this), amount2);
         if(totalLiquidity > 0 ) {
             uint liquidity = LiquidityCalc.amountToLiquidity(amount1, balance1(), totalLiquidity);
             liquidityOfAccount[msg.sender] += liquidity; 
@@ -101,12 +95,8 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
         uint amount1 = LiquidityCalc.liquidityToAmount(liquidity, balance1(), totalLiquidity);
         uint amount2 = LiquidityCalc.liquidityToAmount(liquidity, balance2(), totalLiquidity);
         totalLiquidity -= liquidity;
-        (bool success, ) = 
-            liquidityPool.token1.call(abi.encodeWithSelector(IERC20.transfer.selector, address(msg.sender), amount1));
-        require(success,"failed transfer token1");
-        (success, ) = 
-            liquidityPool.token2.call(abi.encodeWithSelector(IERC20.transfer.selector, address(msg.sender), amount2));
-        require(success,"failed transfer token2");
+        TransferHelper.transfer(token1, address(msg.sender), amount1);
+        TransferHelper.transfer(token2, address(msg.sender), amount2);
         emit LiquidityRemoved(amount1, amount2, msg.sender);
     }
 
@@ -114,12 +104,12 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
         require(totalLiquidity>0);
         if(oneToTwo){
             (bool success, bytes memory data) = 
-                liquidityPool.token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(msg.sender)));
+                token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(msg.sender)));
             require(success && abi.decode(data, (uint))>=amount);
 
         } else {
             (bool success, bytes memory data) = 
-                liquidityPool.token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(msg.sender)));
+                token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(msg.sender)));
             require(success && abi.decode(data, (uint))>=amount);
         }
 
@@ -129,20 +119,16 @@ contract LiquidityPool is ReentrancyGuard,ILiquidityPool{
             amount*priceAfterSwap/deliminator;
 
         if(oneToTwo){
-            (bool success,bytes memory data) = liquidityPool.token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+            (bool success,bytes memory data) = token2.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
             require(success && abi.decode(data,(uint))>=toSend,"Not Enought Liquidity");
-            (success,) = liquidityPool.token1.call(abi.encodeWithSignature("transferFrom(address,address,uint256)", address(msg.sender),address(this),amount));
-            require(success,"failed");
-            (success, )= liquidityPool.token2.call(abi.encodeWithSelector(IERC20.transfer.selector,address(msg.sender),toSend));
-            require(success,"failed");
+            TransferHelper.transferFrom(token1 , address(msg.sender), address(this), amount);
+            TransferHelper.transfer(token2, address(msg.sender), toSend);
             emit Swap(amount, toSend, msg.sender, priceAfterSwap);
         } else {
-            (bool success,bytes memory data) = liquidityPool.token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
+            (bool success,bytes memory data) = token1.staticcall(abi.encodeWithSelector(IERC20.balanceOf.selector, address(this)));
             require(success && abi.decode(data,(uint))>=toSend,"Not Enought Liquidity");
-            (success,) = liquidityPool.token2.call(abi.encodeWithSignature("transferFrom(address,address,uint256)", address(msg.sender),address(this),amount));
-            require(success,"failed");
-            (success, )= liquidityPool.token1.call(abi.encodeWithSelector(IERC20.transfer.selector,address(msg.sender),toSend));
-            require(success,"failed");
+            TransferHelper.transferFrom(token2, address(msg.sender), address(this), amount);
+            TransferHelper.transfer(token2, address(msg.sender), toSend);
             emit Swap(toSend, amount, msg.sender, priceAfterSwap);
         }
     }
